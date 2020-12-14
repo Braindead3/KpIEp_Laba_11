6c52dedace857ca6d90f6c261a15dba1784fd16c
Vlad <malosay21@gmail.com>
2020-12-14 08:23:43 +0300

    commit

--- KpIEp_Laba_11/Program.cs
+++ KpIEp_Laba_11/Program.cs
@@ -10,6 +10,16 @@ namespace KpIEp_Laba_11
     {
         static void Main(string[] args)
         {
+            int[] array= new int[30];
+            InputArray(array);
+        }
+
+        private static void InputArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
         }
     }
 }
